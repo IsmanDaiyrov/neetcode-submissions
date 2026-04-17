0408185ae4e81c9f7ddc779e9a8a904b726be43c
Isman Daiyrov <65091188+IsmanDaiyrov@users.noreply.github.com>
2026-04-17 17:16:15 -0500

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l = 0
+        res = 0
+        count = defaultdict(int)
+
+        for r in range(len(s)):
+            # add to counts
+            count[s[r]] += 1
+
+            # if window condition is broken, shift l pointer
+            # window length - max freq of the character from the map = number of required replacements
+            while (r - l + 1) - max(count.values()) > k:
+                count[s[l]] -= 1
+                l += 1
+
+            # update the res
+            res = max(res, r - l + 1)
+        
+        return res
